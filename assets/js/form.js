8ea21981c80e45133a692000adf2da8c61e5d671
ThStranick <ThStranick@gmail.com>
2024-03-27 09:55:51 -0400

    finished functionality, started styling

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,5 @@
 const submit = document.querySelector('#submit')
 
-
 submit.addEventListener('click', function (eventObj) {
     eventObj.preventDefault()
 
@@ -8,10 +7,18 @@ submit.addEventListener('click', function (eventObj) {
     let titleInput = document.getElementById("title").value
     let contentInput = document.getElementById("content").value
 
-    console.log(usernameInput)
+    post = {
+        username: usernameInput,
+        title: titleInput,
+        content: contentInput
+    }
 
-    localStorage.setItem('username', usernameInput)
-    localStorage.setItem('title', titleInput)
-    localStorage.setItem('content', contentInput)
+    const posts = JSON.parse(localStorage.getItem('posts')) || []
+    
+    posts.push(post)
+    
+    localStorage.setItem('posts', JSON.stringify(posts))
+
+    window.location = 'blog.html'
 })
 
